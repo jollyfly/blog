43e28af67ee4b8e7d841b595fc5dba49050a1f21
jolly <jollyfly666@gmail.com>
2018-06-05 23:11:40 +0800

    init commit

--- src/components/manageContainer/ArticleManage.vue
+++ src/components/manageContainer/ArticleManage.vue
@@ -1,10 +1,65 @@
 <template>
-<div>edit</div>
+  <div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
+      <el-table-column prop="name" label="标题" width="120"></el-table-column>
+      <el-table-column prop="province" label="分类" width="120">
+      </el-table-column>
+      <el-table-column prop="city" label="标签" width="120"></el-table-column>
+      <el-table-column prop="address" label="内容" width="300"></el-table-column>
+      <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ArticleManage'
+  name: 'ArticleManage',
+  methods: {
+    handleClick (row) {
+      console.log(row)
+    }
+  },
+
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }]
+    }
+  }
 }
 </script>
 
